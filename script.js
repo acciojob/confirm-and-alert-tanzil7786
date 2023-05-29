6837ab1c8c0bebca4d163356a67eb9d10f70cbf0
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-05-30 00:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let test= confirm("Do you want to proceed?");
+if(test===true){
+		alert("You clicked OK. Proceeding...")
+ }else{
+	alert("You clicked Cancel. Exiting...")
+ }
+
+
+
+
